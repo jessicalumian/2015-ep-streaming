e05cd0618c0e3cd3a7ade9e0b082bcd7e702cbf7
jessicamizzi <jessica.mizzi@gmail.com>
2015-11-02 15:06:59 -0800

    cleaning up repo

--- streaming/streaming-benchmark.sh
+++
@@ -1,4 +0,0 @@
-zcat /mnt/work/*.pe.qc.fq.gz | \
-        normalize-by-median.py -k 20 -C 20 -M 1e8 - -o - | \
-        trim-low-abund.py -k 20 -Z 20 -C 3 - -o - -M 1e8 | \
-	extract-paired-reads.py --gzip  -p paired.gz -s single.gz
